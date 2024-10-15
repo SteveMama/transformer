c8f5a3eca9f757b9cf0a31a840cc9f850e150df3
SteveMama <pkompally@gmail.com>
2024-10-15 16:39:53 -0400

    --fixed errors, rewrote Self-Attention Class--

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 class SelfAttention(nn.Module):
     def __init__(self, embed_size, heads):
         super(SelfAttention, self).__init__()
@@ -9,46 +8,39 @@ class SelfAttention(nn.Module):
         self.heads = heads
         self.head_dim = embed_size // heads
 
-        assert (self.head_dim * heads == embed_size), "Embed size need to be div by heads"
+        assert self.head_dim * heads == embed_size, "Embed size must be divisible by heads."
 
-        self.values = nn.Linear(self.head_dim, self.head_dim, bias = False)
-        self.keys = nn.Linear(self.head_dim, self.head_dim, bias= False)
-        self.queries = nn.Linear(self.head_dim, self.head_dim, bias = False)
-        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
-
+        self.values = nn.Linear(embed_size, embed_size, bias=False)
+        self.keys = nn.Linear(embed_size, embed_size, bias=False)
+        self.queries = nn.Linear(embed_size, embed_size, bias=False)
+        self.fc_out = nn.Linear(embed_size, embed_size)
 
     def forward(self, values, keys, query, mask):
-        N = query.shape[0] #how many examples are sent in a single instance of time
-        value_len, key_len, query_len =  values.shape[1], keys.shape[1], query.shape[1]
+        N = query.shape[0]  # Batch size
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
         values = self.values(values)  # (N, value_len, embed_size)
         keys = self.keys(keys)  # (N, key_len, embed_size)
         queries = self.queries(query)  # (N, query_len, embed_size)
 
-        # split embedding into self.heads pieces
-        values = values.reshape(N, value_len, self.heads, self.head_dim)
-        keys = keys.reshape(N,key_len, self.heads, self.head_dim)
-        queries = queries.reshape(N, query_len, self.heads, self.head_dim)
+        values = values.view(N, value_len, self.heads, self.head_dim)
+        keys = keys.view(N, key_len, self.heads, self.head_dim)
+        queries = queries.view(N, query_len, self.heads, self.head_dim)
 
         energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
-        # queries shape: (N, query_len, heads, heads_dim)
-        # keys shape: (N, key_len, head, heads_dim)
-        # energy shape: (N, heads, query_len, key_len)
 
         if mask is not None:
-            energy = energy.masked_fill(mask == 0, float("-1e20")) #setting value approach -ve inf
+            energy = energy.masked_fill(mask == 0, float("-1e20"))  # -inf for masked positions
 
-        attention = torch.softmax(energy / (self.embed_size ** (1/2)) , dim=3)
+        attention = torch.softmax(energy / (self.head_dim ** 0.5), dim=3)
 
         out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
-            N, query_len, self.heads*self.head_dim
+            N, query_len, self.heads * self.head_dim
         )
-        # attention shape: (N, heads, query_len, key_len)
-        # values shape: (N, value_len, heads, heads_dim)
-        # after einsum (N, query_len, heads, head_dim) then flatten last two dimension
 
         out = self.fc_out(out)
         return out
+
 
 class TransformerBlock(nn.Module):
     def __init__(self, embed_size, heads, dropout, forward_expansion):
@@ -101,7 +93,7 @@ class Encoder(nn.Module):
                     dropout = dropout,
                     forward_expansion=forward_expansion,
                 )
-            ]
+            for _ in range(num_layers)]
         )
         self.dropout = nn.Dropout(dropout)
 
@@ -169,6 +161,7 @@ class Decoder(nn.Module):
             x = layer(x, enc_out, enc_out, src_mask, target_mask)
 
         out = self.fc_out(x)
+        return out
 
 class Transformer(nn.Module):
     def __init__(self,
